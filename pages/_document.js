eaf356dd11a2a3d8a2e37902ef2b782be00732f6
SmiKo9396 <cjs@gle.bm>
2025-08-19 13:28:11 -0300

    confirm favicon chnages

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* your existing tags */}
-         <link rel="shortcut icon" href="/favicon-16x16.png" />
+         <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
